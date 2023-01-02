83734c5608706c44f36a3da769534fc08ee83daf
Laurent BOSSAVIT <216452+Morendil@users.noreply.github.com>
2023-01-02 17:27:25 +0100

    Day 01 part 2

--- Day01.hs
+++ Day01.hs
@@ -1,6 +1,7 @@
 import Data.Char ( digitToInt )
+import Data.Function.HT
 
--- | Day 01
+-- | Day 01 part 1
 --
 -- >>> captcha "1122"
 -- 3
@@ -16,6 +17,26 @@ captcha digits = check digits
     where check = sum . map fst . filter (uncurry (==)) . pairs . map digitToInt
           pairs digits = zip digits (tail $ cycle digits)
 
+-- | Day 01 part 2
+--
+-- >>> captcha2 "1212"
+-- 6
+-- >>> captcha2 "1221"
+-- 0
+-- >>> captcha2 "123425"
+-- 4
+-- >>> captcha2 "123123"
+-- 12
+-- >>> captcha2 "12131415"
+-- 4
+
+captcha2 :: String -> Int
+captcha2 digits = check digits
+    where check = sum . map fst . filter (uncurry (==)) . pairs . map digitToInt
+          pairs digits = zip digits (nest halfway tail $ cycle digits)
+            where halfway = length digits `div` 2
+
 main = do
     input <- readFile "day01.txt"
     print $ captcha input
+    print $ captcha2 input
