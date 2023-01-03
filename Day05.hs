748641e38b6b8ca074773d42385f091196a5c05d
Laurent Bossavit <laurent.git@bossavit.com>
2023-01-03 23:06:30 +0100

    Day 5 part 2

--- Day05.hs
+++ Day05.hs
@@ -2,14 +2,22 @@ import qualified Data.IntMap as M
 import Data.Maybe ( fromJust )
 
 part1 :: [Int] -> Int
-part1 offsets = length $ takeWhile ((>)n . fst) $ iterate step (0, mapped)
+part1 = solve succ
+
+part2 :: [Int] -> Int
+part2 = solve rule2
+    where rule2 cur = if cur >= 3 then pred cur else succ cur
+
+solve :: (Int -> Int) -> [Int] -> Int
+solve fn offsets = length $ takeWhile ((>)n . fst) $ iterate (step fn) (0, mapped)
     where n = length offsets
           mapped = M.fromList $ zip [0..] offsets
 
-step :: (Int, M.IntMap Int) -> (Int, M.IntMap Int)
-step (cur, offsets) = (next, M.adjust succ cur offsets)
+step :: (Int -> Int) -> (Int, M.IntMap Int) -> (Int, M.IntMap Int)
+step fn (cur, offsets) = (next, M.adjust fn cur offsets)
     where next = cur + fromJust (M.lookup cur offsets)
 
 main = do
     offsets <- map read . lines <$> readFile "day05.txt"
     print $ part1 offsets
+    print $ part2 offsets
