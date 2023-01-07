2f5932cf25e8aa17d280f904e3ec09e37ab181bf
Laurent Bossavit <laurent.git@bossavit.com>
2023-01-07 12:19:23 +0100

    Day 12 part 2

--- Day12.hs
+++ Day12.hs
@@ -1,12 +1,20 @@
 import Data.Char (isDigit, isSpace)
-import Data.Graph (graphFromEdges, reachable)
+import Data.Graph (graphFromEdges, reachable, components, Graph)
 
 part1 :: [[Int]] -> Int
-part1 pipes = length $ reachable graph 0
+part1 = length . (`reachable` 0) . toGraph
+
+part2 :: [[Int]] -> Int
+part2 = length . components . toGraph
+
+toGraph :: [[Int]] -> Graph
+toGraph pipes = graph
     where (graph, nodeFromVertex, vertexFromKey) = graphFromEdges relations
           relations = map rel pipes
           rel (x:rest) = (x,x,rest)
 
+
 main = do
-    pipes <- map (map read . words . filter (\c -> isDigit c || isSpace c)) . lines <$> readFile "day12.txt"
+    pipes <- map (map read . words . filter (\c -> isDigit c || isSpace c)) . lines <$> readFile "day12.txt"
     print $ part1 pipes
+    print $ part2 pipes
