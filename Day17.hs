5feaf3054ed89a881768e1f50173fe4f8da21e5d
Laurent Bossavit <laurent.git@bossavit.com>
2023-01-08 19:20:57 +0100

    Day 17 part 2

--- Day17.hs
+++ Day17.hs
@@ -14,5 +14,14 @@ part1 :: Int -> Int
 part1 step = fromJust $ ring S.!? (pos + 1)
     where (ring, pos, val) = last $ take 2018 $ iterate (insertion step) initial
 
+part2 :: Int -> Int -> Int
+part2 count step = go count (0,1) 0
+    where go 0 _ found = found
+          go n (pos, val) lastFound = go (n-1) (newPos,val+1) maybeFound
+                where newPos = 1 + (pos+step) `mod` val
+                      maybeFound = if newPos == 1 then val else lastFound
+
+
 main = do
     print $ part1 376
+    print $ part2 50000000 376
