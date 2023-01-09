3306e8c00696a4ba5d597c696e43dfe63ff35f20
Laurent BOSSAVIT <216452+Morendil@users.noreply.github.com>
2023-01-09 16:44:28 +0100

    Day 19 part 2

--- Day19.hs
+++ Day19.hs
@@ -5,8 +5,9 @@ import Data.Char (isAlpha)
 
 type Point = (Int, Int)
 type Direction = Point
-type Packet = (Point, Direction)
+type Packet = (Point, Direction,Int)
 type Network = M.Map Point Char
+type Result = (String, Packet)
 
 neighbours :: [Direction]
 neighbours = [(-1,0),(1,0),(0,-1),(0,1)]
@@ -17,34 +18,40 @@ anti (x,y) = (-x,-y)
 start :: Network -> Point
 start = head . filter (\(x,y) -> y == 0) . M.keys
 
-collect :: Network -> Packet -> String
+collect :: Network -> Packet -> Result
 collect network = go "" . move network
-    where cell (pos,_) = M.findWithDefault ' ' pos network 
-          go acc packet | cell packet == ' ' = acc
+    where cell (pos,_,_) = M.findWithDefault ' ' pos network
+          go acc packet | cell packet == ' ' = (acc,packet)
           go acc packet | cell packet == '+' = go acc (move network $ turn network packet)
           go acc packet | isAlpha (cell packet) = go (acc++[cell packet]) (move network packet)
           go _ packet = error $ "Packet stopped at unexpected point, " ++ show packet
 
 turn :: Network -> Packet -> Packet
-turn network (pos,dir) = (pos, head (directions \\ [anti dir]))
+turn network (pos,dir,steps) = (pos, head (directions \\ [anti dir]),steps)
     where directions = filter (\offset -> M.findWithDefault ' ' (add pos offset) network /= ' ') neighbours
 
 move :: Network -> Packet -> Packet
 move network packet = last $ takeUntil atStop $ drop 1 $ iterate step packet
     where atStop p = cell p == '+' || cell p == ' ' || isAlpha (cell p)
-          cell (pos,_) = M.findWithDefault ' ' pos network
+          cell (pos,_,_) = M.findWithDefault ' ' pos network
 
 step :: Packet -> Packet
-step (pos,dir) = (add dir pos, dir)
+step (pos,dir,steps) = (add dir pos, dir, steps+1)
 
 add (x1,y1) (x2,y2) = (x1+x2,y1+y2)
 
 scan :: [String] -> Network
 scan lines = M.fromList [((x,y), cell) | x <- [0..length (head lines)-1], y <- [0..length lines-1], let cell = (lines !! y) !! x, cell /= ' ']
 
-part1 :: Network -> String
-part1 network = collect network (start network,(0,1))
+part1 :: Result -> String
+part1 = fst
+
+part2 :: Result -> Int
+part2 = getSteps . snd
+    where getSteps (_,_,steps) = steps
 
 main = do
     diagram <- scan . lines <$> readFile "day19.txt"
-    print $ part1 diagram
+    let result = collect diagram (start diagram,(0,1),0)
+    print $ part1 result
+    print $ part2 result
